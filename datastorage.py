10da2cedcb1a8fb60782b5ab1e69097bd3cd3444
nightblooda <44409710+nightblooda@users.noreply.github.com>
2019-12-13 00:00:04 +0530

    Update datastorage.py

--- datastorage.py
+++ datastorage.py
@@ -1,1 +1,11 @@
 
+import pymysql
+import config
+from pymysql import cursors
+
+class sqloperater:
+    def __init__(self, sqlstr):
+        super().__init__()
+        self.sqlstr = sqlstr
+        self.status = ''
+        
